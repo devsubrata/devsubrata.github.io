06a8d85ac70024739e2e8d7a55107a3eeef68c4c
devsubrata <63883576+devsubrata@users.noreply.github.com>
2021-07-19 08:29:06 +0600

    Add files via upload

---
+++ vocabulary_FlashCard/script.js
@@ -0,0 +1,20 @@
+
+/*
+Array.from(document.querySelectorAll(".front")).forEach((item) => {
+
+    item.onclick = () => {
+
+        item.classList.toggle("change");
+    };
+
+});
+
+Array.from(document.querySelectorAll(".back")).forEach((item) => {
+
+    item.onclick = () => {
+
+        item.classList.toggle("change");
+    };
+
+});
+*/
