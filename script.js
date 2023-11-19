ef95001e4efe4e573e28d17d9673f72161d2bd1f
devsubrata <devsubrata@gmail.com>
2023-11-20 05:48:42 +0600

    clock replacement for responsive desing

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const quoteContainer = document.querySelector('.quote-container');
 const quote = document.querySelector('.quote');
 const author = document.querySelector('.author');
 const category = document.querySelector('.category');
+const plainClock = document.querySelector('.plain-clock');
+
 
 const getQuote = async () => {
     const response = await fetch("https://api.api-ninjas.com/v1/quotes", {headers: {'X-Api-Key': '3Jv07cnSjRyZ3pTvRvHUYw==A6Vz5B5Fu9IQybsS'} });
@@ -14,6 +16,19 @@ const getQuote = async () => {
 }
 
 getQuote();
+
+
+const ShowPlainClock = () => {
+    const today = new Date();
+    const timetoday = today.toLocaleString('en-GB').split(',');
+    const clock = `
+        <h1>${timetoday[1]}</h1>
+        <h2>${timetoday[0]}</h2>
+    `;
+    plainClock.innerHTML = clock;
+}
+
+setInterval(ShowPlainClock, 1000);
 
 
 
